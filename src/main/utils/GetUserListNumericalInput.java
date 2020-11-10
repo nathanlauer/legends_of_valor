0cef02b3d1e0519a5004b92fc9cbcdeeaff64e21
Nathan Lauer <lauern91@gmail.com>
2020-11-10 07:58:15 -0500

    made three of four user input classes subclasses of GetUSerInput

--- src/main/utils/GetUserListNumericalInput.java
+++ src/main/utils/GetUserListNumericalInput.java
@@ -17,10 +17,9 @@ import java.util.regex.Pattern;
  * <p>
  * Please feel free to ask me any questions. I hope you're having a nice day!
  */
-public class GetUserListNumericalInput {
+public class GetUserListNumericalInput extends GetUserInput {
     private String prompt;
     private List<String> options;
-    private final Scanner scanner;
 
     /**
      * Empty constructor
@@ -46,7 +45,7 @@ public class GetUserListNumericalInput {
      * @param options the List of options to present to the user
      */
     public GetUserListNumericalInput(Scanner scanner, String prompt, List<String> options) {
-        this.scanner = scanner;
+        super(scanner);
         this.prompt = prompt;
         this.options = options;
     }
@@ -104,7 +103,8 @@ public class GetUserListNumericalInput {
 
             // Now, get their input
             try {
-                String input = scanner.nextLine();
+                this.getNextLine();
+                String input = this.getUserInput();
                 String[] values = input.split(",\\s*");
                 for(String value : values) {
                     try {
